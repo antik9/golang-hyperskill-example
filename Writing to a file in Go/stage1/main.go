88171dd47cb18efc238c155cf01cfa5a651e2432
Illarionov Anton <illarionovanton@gmail.com>
2022-03-03 22:00:52 +0300

    feat: add example of the go hyperskill task

---
+++ Writing to a file in Go/stage1/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func main() {
+	quote := readQuote() // today's quote
+	file, err := os.Create("quote.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// TODO: write the quote to a file
+	file.WriteString(quote)
+}
+
+func readQuote() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanLines)
+	_ = scanner.Scan()
+	return scanner.Text()
+}
